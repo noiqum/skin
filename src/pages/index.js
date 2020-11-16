8df59f43978b52236cfda4807724a6259a9c5007
unknown <onurcoskun99@gmail.com>
2020-11-16 18:43:25 +0300

    layout component and mdx configuration done

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
+import Layout from '../../components/layout';
 
-export default () => <div>Hello gatsby</div>
+export default () => {
+    return (<Layout>
+        <h1>title</h1>
+        <div>Hello gatsby</div>
+    </Layout>)
+}
